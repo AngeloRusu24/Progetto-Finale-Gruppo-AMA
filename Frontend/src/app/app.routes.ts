4ab78b4e7bf8324d12a8191e1b23e6b1f9d4d68a
mariadelvecchio002-bit <mariadelvecchio002@gmail.com>
2026-06-16 14:56:28 +0200

    Pagina Profile

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
+import { ProfileComponent } from './pages/profile/profile';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent},
+  { path: 'profile', component: ProfileComponent }
 ];
--- Frontend/src/app/pages/profile/profile.ts
+++ Frontend/src/app/pages/profile/profile.ts
@@ -1,11 +1,57 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './profile.html',
-  styleUrl: './profile.css',
+  styleUrl: './profile.css'
 })
-export class Profile {
+export class ProfileComponent {
 
+  // dati utente (mock per ora, poi arriveranno dal backend)
+  user = {
+    username: 'Maria',
+    email: 'maria@email.com',
+    createdAt: 'Gennaio 2025'
+  };
+
+  // ricette dell’utente
+  myRecipes = [
+    {
+      title: 'Carbonara',
+      category: 'Primi',
+      time: 20,
+      difficulty: 'Media',
+      rating: 4.8
+    },
+    {
+      title: 'Tiramisù',
+      category: 'Dolci',
+      time: 40,
+      difficulty: 'Facile',
+      rating: 4.9
+    },
+    {
+      title: 'Pollo al forno',
+      category: 'Secondi',
+      time: 60,
+      difficulty: 'Media',
+      rating: 4.5
+    }
+  ];
+
+  // migliori ricette (per ora prese manualmente)
+  bestRecipes = [
+    this.myRecipes[1],
+    this.myRecipes[0]
+  ];
+
+  // 💬 commenti ricevuti
+  comments = [
+    { user: 'Marco', text: 'Ricetta fantastica!', rating: 5, recipeTitle: "Carbonara" },
+    { user: 'Anna', text: 'Molto facile da seguire', rating: 4, recipeTitle: "Tiramisù" },
+    { user: 'Luca', text: 'Ottimo risultato!', rating: 5, recipeTitle: "Tiramisù" }
+  ];
 }
