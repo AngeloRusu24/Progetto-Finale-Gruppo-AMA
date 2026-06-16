7097947a6e47ce68e94df853a8a2ba50272f0c8f
mariadelvecchio002-bit <mariadelvecchio002@gmail.com>
2026-06-16 16:12:49 +0200

    settato errore nella route

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,20 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
 import { ProfileComponent } from './pages/profile/profile';
-
+import { Register } from './pages/register/register';
 import { RecipesComponent } from './pages/recipes/recipes';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'profile', component: ProfileComponent },
-  { path: 'recipes', component: RecipesComponent }
+  { path: 'recipes', component: RecipesComponent },
+  { path: 'register', component: Register }
 
-import { Register } from './pages/register/register';
-
-export const routes: Routes = [
-
-  { path: '', component: HomeComponent },
-  { path: 'profile', component: ProfileComponent },
-  {path:'register', component:Register}
-
-];
+]
