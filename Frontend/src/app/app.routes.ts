58d0615764080b82c80754bad52da803d4ecb192
mariadelvecchio002-bit <mariadelvecchio002@gmail.com>
2026-06-17 10:50:11 +0200

    MDV css migliorato e addrecipecomponent

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,11 +4,14 @@ import { ProfileComponent } from './pages/profile/profile';
 import { Register } from './pages/register/register';
 import { RecipesComponent } from './pages/recipes/recipes';
 import { Login } from './pages/login/login';
+import { AddRecipeComponent } from './pages/add-recipe/add-recipe';
+
 
 export const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'profile', component: ProfileComponent },
   { path: 'recipes', component: RecipesComponent },
+  { path: 'recipes/new', component: AddRecipeComponent },
   {path:'register', component:Register},
   {path:'login', component:Login}
 
