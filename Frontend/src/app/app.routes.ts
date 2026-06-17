36cf9de143d6061bd07a80dd539be56b34d97ec5
AngeloRusu24 <angelorusu24@gmail.com>
2026-06-17 11:29:14 +0200

    sistemato app routes

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,20 +4,15 @@ import { ProfileComponent } from './pages/profile/profile';
 import { Register } from './pages/register/register';
 import { RecipesComponent } from './pages/recipes/recipes';
 import { Login } from './pages/login/login';
-<<<<<<< Updated upstream
 import { AddRecipeComponent } from './pages/add-recipe/add-recipe';
-
-=======
 import { RecipeDetail } from './pages/recipe-detail/recipe-detail';
->>>>>>> Stashed changes
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent},
+  { path: '', component: HomeComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'recipes', component: RecipesComponent },
   { path: 'recipes/new', component: AddRecipeComponent },
-  {path:'register', component:Register},
-  {path:'login', component:Login},
+  { path: 'register', component: Register },
+  { path: 'login', component: Login },
   { path: 'recipe/:id', component: RecipeDetail }
-
-]
+];
