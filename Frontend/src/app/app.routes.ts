6157755d74beadf1d556f827f04fa4838f773a98
AngeloRusu24 <angelorusu24@gmail.com>
2026-06-18 12:00:39 +0200

    sistemato alcune cose di login

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -5,15 +5,15 @@ import { Register } from './pages/register/register';
 import { RecipesComponent } from './pages/recipes/recipes';
 import { Login } from './pages/login/login';
 import { AddRecipeComponent } from './pages/add-recipe/add-recipe';
-
 import { RecipeDetail } from './pages/recipe-detail/recipe-detail';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'recipes', component: RecipesComponent },
-  { path: 'recipes/new', component: AddRecipeComponent },
+  { path: 'recipes/new', component: AddRecipeComponent, canActivate: [authGuard] },
   { path: 'register', component: Register },
   { path: 'login', component: Login },
-  { path: 'recipe/:id', component: RecipeDetail }
+  { path: 'recipe/:id', component: RecipeDetail, canActivate: [authGuard] },
 ];
